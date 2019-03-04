e89b43013a28b562ca2ca03c8ac7f915851e8a50
Ubuntu <ubuntu@ip-10-0-0-214.ap-northeast-2.compute.internal>
2019-03-04 04:49:08 +0000

    merged proof

--- chat/urls.py
+++ chat/urls.py
@@ -3,10 +3,6 @@ from chat import views
 
 app_name = 'chat'
 
-from chat import models
-
-models.Chatroom.objects.all().delete()  # clear all chatrooms that could remain becasue of abnormal termination
-
 urlpatterns = [    
     path('', views.IndexView.as_view(), name='index'),
     path('join/', views.IndexView.as_view(), name='join'),
